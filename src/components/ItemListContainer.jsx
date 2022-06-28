f2affd5f8391fc729439dc4cb038ab0e072b8aa6
Gonzalo Martinez <gonzalomartinezadalid@gmail.com>
2022-06-28 18:35:30 -0300

    ItemCount

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,16 @@
+import ItemCount from "./ItemCount";
 import React from "react";
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (quantity) => {
+      console.log(`Usted quiere ${quantity} unidades`)
+  }
+
   return (
     <div className="landing">
       <span className=" flex flex-1 items-center justify-center">{greeting}</span>
+      <ItemCount initial={1} stock={10} onAdd={onAdd}/>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <div className="navbar h-[60px] shadow-md relative z-10">
       <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb-[10px] 
         flex justify-between items-center">
-            {/*left div */}
+{/*left div */}
         <div className="left flex flex-1 item-center">
           <div className="searchInput flex border-[2px] border-solid 
           border-lighgrey rounded-md items-center ml-[10px] p-[5px] 
@@ -25,7 +25,7 @@ function Navbar() {
         <div className="center flex-1 text-center" >
             <div className="logo font-bold text-lg">Ame Tienda de Arte</div>
         </div>
-        {/* Right div */}
+{/* Right div */}
         <div className="right flex flex-1 items-center justify-end">
             <div className={style}>Registrarse</div>
             <div className={style}>Acceder</div>
